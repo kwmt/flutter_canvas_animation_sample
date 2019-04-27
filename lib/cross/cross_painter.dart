bd210ca10cfbab02b029b57580276d575e192e6d
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-04-27 20:07:43 +0900

    左→右とアニメーションするように変更

--- lib/cross/cross_painter.dart
+++ lib/cross/cross_painter.dart
@@ -15,10 +15,25 @@ class CrossPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     print("paint $_fraction");
+
+    double leftFraction;
+    double rightFraction;
+
+    if (_fraction < .5) {
+      leftFraction = _fraction / .5;
+      rightFraction = 0;
+    } else {
+      leftFraction = 1.0;
+      rightFraction = (_fraction - .5) / .5;
+    }
+
     canvas.drawLine(Offset(0, 0),
-        Offset(size.width * _fraction, size.height * _fraction), _paint);
-    canvas.drawLine(Offset(size.width, 0),
-        Offset(size.width - size.width * _fraction, size.height * _fraction), _paint);
+        Offset(size.width * leftFraction, size.height * leftFraction), _paint);
+    canvas.drawLine(
+        Offset(size.width, 0),
+        Offset(size.width - size.width * rightFraction,
+            size.height * rightFraction),
+        _paint);
   }
 
   @override
