71bd53466419fbc469cfe8c6bf1c510e3d83af32
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-04-27 13:39:00 +0900

    first commit create new project

---
+++ android/app/src/main/kotlin/net/kwmt27/flutter_canvas_animation/flutter_canvas_animation/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.kwmt27.flutter_canvas_animation.flutter_canvas_animation
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
